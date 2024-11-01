346e9d1ba836a4d19593d7a78c975583f70bc1aa
YakeDev <erickay.dev@gmail.com>
2024-11-01 18:52:07 +0200

    Installation de tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/styles/index.css", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+      gridTemplateColumns: {
+        "70/30": "70% 28%",
+      },
+    },
+  },
+  plugins: [],
+};
